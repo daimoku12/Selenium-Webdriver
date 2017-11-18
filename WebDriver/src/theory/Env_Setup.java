fe65c7b79fe4379c9f511c0885a865a8864f48eb
Kannu <Kannu@192.168.1.4>
2017-11-18 10:38:40 +0530

    Jenkins added

--- WebDriver/src/theory/Env_Setup.java
+++ WebDriver/src/theory/Env_Setup.java
@@ -138,52 +138,9 @@ http://cucumber.github.com/cucumber-eclipse/update-site
  
 16) Cucumber - Junit version 4.11...........................................
  
-17 Jenkins installation and setup..................................................
+
  
-Google download Jenkins war file
-https://updates.jenkins-ci.org/download/war/
-permalink to the latest
-command prompt
-cd Downloads\Jenkins
-java -jar jenkins.war or java -jar "jenkins.war" --httpPort=8085 (any port)
-Admin user created 
-password: 0c4d00d6a8b547d5bb78f477cfaaffd7
-(C:\Users\Kannu\.jenkins\secrets\initialAdminPassword)
-goto localhost:8080
-Enter Administrator password
-Install default plugins
 
-Manage Jenkins
-Manage plugins
-available - install GitHub, Maven, TestNG Report and Cucumber plugins
-
-Manage Jenkins
-Global tool configuration
-JDK > Add JDK > uncheck automatic installation and provide JDK path set as env variable
-MAVEN > download Maven and extract > Add MAVEN (same steps as JDK) (C:\Users\Kannu\Downloads\apache-maven-3.5.2\bin)
-GIT > Download GIT and install > Add to system variable and Jenkins GIT (C:\Program Files\Git\bin\git.exe)
-
-Create New Job
-Goto New Item > Enter a name > select Maven project > ok > 
-Project Confg > Enter Git Repository url, goals (clean install), POM (Framework\pom.xml),  
-
-Post build action > Publish HTML Reports > Index page(s) - Framework\Report\extent.html
-
-Setting to get extent report in good format (install HTML report plugin)
-Manage Jenkins > Script Console
-System.setProperty("hudson.model.DirectoryBrowserSupport.CSP","")
-System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", 
-"sandbox allow-scripts; default-src 'self'; script-src * 'unsafe-eval'; img-src *; style-src * 'unsafe-inline'; font-src *");
-Run twice until you see result:
-
-Email Setting
-
-Manage Jenkins> Configure System > Email Notification
-SMTP server - smtp.gmail.com
-Use SMTP Authentication (check)
-USE SSL check
-SMTP port - 465 (for gmail)
-Reply to address - noreply@gmail.com
   
 */
 
